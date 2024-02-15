cdcc4fe4984c44013e9de33d3ee802964f6ba2be
Hariraam Kethisvarasuthan <62831820+hraamk@users.noreply.github.com>
2024-02-15 13:02:40 +0530

    buyTicket

--- src/PlaneManagement.java
+++ src/PlaneManagement.java
@@ -7,10 +7,85 @@ public class PlaneManagement {
         System.out.println("Please input row letter :");
         String seatRow = seatInput.nextLine();
 
-        System.out.println("Please input seat number :");
-        String seatNumber = seatInput.nextLine();
+        System.out.println("Please input row number :");
+        int seatNumber = seatInput.nextInt();
+
+        int seatIndex = seatNumber - 1;
+
+        switch(seatRow.toUpperCase()){
+
+            case "A":
+                try {
+                    if (rowA[seatIndex] == 0) {
+                        rowA[seatIndex] = 1;
+                        System.out.println("Seat A" + seatNumber + "  booked successfully.");
+                    }
+                    else {
+                        System.out.println("Seat A" + seatNumber + "  is not available.");
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Invalid seat number");
+
+                    }
+                break;
+
+            case "B":
+                try {
+                    if (rowB[seatIndex] == 0) {
+                        rowB[seatIndex] = 1;
+                        System.out.println("Seat B" + seatNumber + "  booked successfully.");
+                    }
+                    else {
+                        System.out.println("Seat B" + seatNumber + "  is not available.");
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Invalid seat number");
+
+                }
+                break;
+            case "C":
+                try {
+                    if (rowC[seatIndex] == 0) {
+                        rowC[seatIndex] = 1;
+                        System.out.println("Seat C" + seatNumber + "  booked successfully.");
+                    }
+                    else {
+                        System.out.println("Seat C" + seatNumber + "  is not available.");
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Invalid seat number");
+
+                }
+                break;
+
+            case "D":
+                try {
+                    if (rowD[seatIndex] == 0) {
+                        rowD[seatIndex] = 1;
+                        System.out.println("Seat D" + seatNumber + "  booked successfully.");
+                    }
+                    else {
+                        System.out.println("Seat D" + seatNumber + "  is not available.");
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Invalid seat number");
+
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Seat Row");
+                break;
+
+        }
+
 
     }
+
     static void displayMenu() {
 
         System.out.println("Welcome to the Plane Management Application");
@@ -57,10 +132,25 @@ public class PlaneManagement {
 
         displayMenu();
 
+
         System.out.println("\nPlease select an option :");
         int userInput = menuInput.nextInt();
 
+        if(userInput==1){
+            buy_seat(rowA,rowB,rowC,rowD);
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+
+
+        //for (int i = 0; i < rowA.length; i++) {
+        //   System.out.print(rowA[i] + " ");
+        //}
+
     }
+
+
 
 
 
